c14273ff38578ddd2b880f8310aec250783d2d10
druhithad <druhithad@gmail.com>
2026-04-17 15:03:08 +0530

    day3 afternoon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,12 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Register from './components/Register'
 import Login from './components/Login'
+import Addproduct from './components/Addproduct'
+import Home from './components/Home'
 import ProtectedRoute from './components/ProtectedRoute'
+import Cart from './components/Cart'
+
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -17,6 +22,12 @@ export default function App() {
             <Addproduct/>
           </ProtectedRoute>
         }/>
+        <Route path="/cart" element={
+          <ProtectedRoute>
+            <Cart/>
+          </ProtectedRoute>
+        }/>
+
       </Routes>
     </BrowserRouter>
   )
